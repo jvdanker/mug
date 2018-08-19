e9f91390cfb0ab4590385e27896472b1f56516d0
Juan van den Anker <juan@vdanker.net>
2018-08-19 20:38:21 +1200

    refactoring

--- mugweb/src/App.js
+++ mugweb/src/App.js
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            urls: [],
+            url: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.add = this.add.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({url: event.target.value});
+    }
+
+    add(event) {
+        event.preventDefault();
+
+        const urls = this.state.urls;
+        urls.push(this.state.url);
+
+        this.setState({
+            urls: urls
+        });
+    }
+
+    render() {
+        const listItems = this.state.urls.map((url, index) =>
+            <li key={index}>
+                {url}
+            </li>
+        );
+
+        return (
+            <div className="App">
+                <form onSubmit={this.add}>
+                    <fieldset>
+                        <legend>Add URL</legend>
+
+                        <label htmlFor="url">Enter an URL:</label>
+                        <input type="url"
+                               name="url"
+                               placeholder="https://example.com"
+                               pattern="(http(s?)://?).*"
+                               size="20"
+                               value={this.state.url}
+                               onChange={this.handleChange}
+                               required/>
+                    </fieldset>
+
+                    <button type="submit">Add URL</button>
+                </form>
+
+                <ul>{listItems}</ul>
+            </div>
+        );
+    }
 }
 
 export default App;
